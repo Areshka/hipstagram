67dab74946e8e4f1f62087956abcc6422f7b5fc0
Oleg M <oleg.moskalenko@gmail.com>
2020-09-15 16:45:45 +0300

    import Auth container

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
 import React from 'react';
-import { ReactComponent as Logo } from './images/logo.svg'
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Wrapper from './components/Wrapper/Wrapper';
+import Auth from './containers/Auth';
 
 import './App.scss';
-import FormInput from './components/FormInput/FormInput';
 
 function App() {
   return (
-    <Wrapper>
-      <div className="wrap"></div>
-      <div className="login">
-        <h1 className="logo">
-          <Logo className="logo__img" />
-          <span className="logo__title">hipstagram</span>
-        </h1>
-        <form className="form-auth">
-          <h3>Sing Up</h3>
-          <FormInput
-            label="Login"
-            className="input"
-            type="text"
-            name="login"
-            placeholder="Enter your login"
-          />
-          <FormInput
-            label="Email"
-            className="input"
-            type="email"
-            name="email"
-            placeholder="Enter your Email"
-          />
-          <FormInput
-            label="Password"
-            className="input"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-          />
-        </form>
-      </div>
-    </Wrapper>
+    <div className="wrapper">
+      <Router>
+        <Auth />
+      </Router>
+    </div>
   );
 }
 
