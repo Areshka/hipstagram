486e7247223aabbe955d0d0f26ebd39058e3708a
Oleg M <oleg.moskalenko@gmail.com>
2020-09-16 16:56:07 +0300

    added component Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Auth from './containers/Auth';
+import Header from './components/Header';
 
 import './App.scss';
 
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Auth />
       </Router>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Wrapper from '../Wrapper';
+import FormInput from '../FormInput';
+
+import { ReactComponent as IconUser } from '../../images/icon_user.svg';
+import { ReactComponent as IconLogout } from '../../images/icon_logout.svg';
+import './style.scss';
+
+const Header = () => {
+  return (
+    <header className="main-header">
+      <Wrapper>
+        <div className="main-header__inner">
+          <form className='form-search'>
+            <FormInput
+              className="form-serch__input"
+              type="serch"
+              name="serch"
+              placeholder="Enter login"
+            />
+          </form>
+          <div className="main-header__title">
+            Finded 3 users
+          </div>
+          <ul className="user-bl">
+            <li><IconUser /></li>
+            <li>
+              <button
+                className="bnt-logout"
+                onClick={() => { console.log("Logout") }}
+              >
+                <IconLogout id="logout" />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </Wrapper>
+    </header>
+  );
+}
+
+
+export default Header;
