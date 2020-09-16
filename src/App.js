354bd442efe4edcf7a271d6339019ca609a31d3a
Areshka <oleg1990moskalenko@gmail.com>
2020-09-17 01:16:49 +0300

    added Users component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 
 import Auth from './containers/Auth';
-import Header from './components/Header';
+import Users from './components/Users';
 
 import './App.scss';
 
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Auth />
       </Router>
-      <Header />
+      <Users />
     </div>
   );
 }
