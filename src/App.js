d67730be37c2de1a329cf992098fc8ff684e0ce0
Areshka <oleg1990moskalenko@gmail.com>
2020-09-17 21:42:17 +0300

    added route to /users

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Auth from './containers/Auth';
 import Users from './components/Users';
@@ -7,12 +7,19 @@ import Users from './components/Users';
 import './App.scss';
 
 function App() {
+  const isAuth = true;
   return (
     <div className="wrapper">
       <Router>
-        <Auth />
+        {isAuth ?
+          <>
+            <Route path='/users' component={Users} />
+            <Redirect to='/users' />
+          </>
+          :
+          <Auth />
+        }
       </Router>
-      <Users />
     </div>
   );
 }
