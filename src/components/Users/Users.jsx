79b199f6efa9eb71cf39315c1a9f21c6b3ca8f91
Oleg M <oleg.moskalenko@gmail.com>
2020-09-17 16:11:35 +0300

    user rendering

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 
-import Header from '../Header/Header';
+import User from './User';
+import Header from '../Header';
+import { WrapperContent } from '../Wrapper/Wrapper'
+import { ReactComponent as UsersNotFound } from '../../images/icon_not_user.svg'
+
+import './style.scss';
 
 const Users = () => {
+  const users = [
+    {
+      name: "Petya",
+    }
+  ]
+
   return (
     <>
       <Header />
-      <div className="wrap">
-
-      </div>
+      <WrapperContent>
+        {users.length ?
+          <div className='users'>
+            <User />
+            <User />
+            <User />
+          </div>
+          :
+          <div className='not-users'>
+            <UsersNotFound />
+            <p>Users not found</p>
+          </div>
+        }
+      </WrapperContent>
     </>
   );
 }
