20269e5594361b0b75a5e9223cadf859bd404310
Areshka <oleg1990moskalenko@gmail.com>
2020-09-22 01:43:56 +0300

    added breakpoint constat

---
+++ src/constants/breakpoint.js
@@ -0,0 +1,15 @@
+const size = {
+  exsm: '320px',
+  sm: '576px',
+  md: '768px',
+  lg: '992px',
+  xl: '1200px'
+}
+
+export const device = {
+  mobileS: `(min-width: ${size.exsm})`,
+  mobileM: `(min-width: ${size.sm})`,
+  tablet: `(min-width: ${size.md})`,
+  laptop: `(min-width: ${size.lg})`,
+  desktop: `(min-width: ${size.xl})`,
+};
