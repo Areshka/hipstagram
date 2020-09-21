7a1012a1e6cefd6aae38a887edbb7a7a052e57a2
Areshka <oleg1990moskalenko@gmail.com>
2020-09-22 01:43:12 +0300

    added styled components for Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 
-import Button from '../Button';
+import { Button } from '../Button/Button';
 import FormInput from '../FormInput';
 
 import { loginValidationData, passwordValidationData } from '../../constants/formPatterns';
+import { FormAuth, FormAuthTitle, AuthLink } from '../../containers/Auth/styled';
+import FieldWrapper from '../FieldWrapper';
 
 const Login = () => {
   const { register, handleSubmit, errors } = useForm();
@@ -16,33 +18,31 @@ const Login = () => {
 
   return (
     <>
-      <form className="form-auth" onSubmit={handleSubmit(onSubmit)}>
-        <h3 className="form-auth__title">Sing In</h3>
-        <FormInput
-          label="Login"
-          className="field-wrap__input"
-          type="text"
-          name="login"
-          placeholder="Enter your login"
-          error={errors.login}
-          reference={register(loginValidationData)}
-        />
-        <FormInput
-          label="Password"
-          className="field-wrap__input"
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-          error={errors.password}
-          reference={register(passwordValidationData)}
-        />
-        <div className="btn-wrapper">
-          <Button className="btn" type="submit" title="Sing In" />
-        </div>
-      </form>
-      <p className="auth-link">
+      <FormAuth onSubmit={handleSubmit(onSubmit)}>
+        <FormAuthTitle>Sing In</FormAuthTitle>
+        <FieldWrapper label="Login" error={errors.login || ''}>
+          <FormInput
+            type="text"
+            name="login"
+            placeholder="Enter your login"
+            invalid={'login' in errors}
+            ref={register(loginValidationData)}
+          />
+        </FieldWrapper>
+        <FieldWrapper mb label="Password" error={errors.password || ''}>
+          <FormInput
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            invalid={'password' in errors}
+            ref={register(passwordValidationData)}
+          />
+        </FieldWrapper>
+        <Button type="submit" title="Sing In" />
+      </FormAuth>
+      <AuthLink>
         If you not have account you can <Link to="/registration">Registration</Link>
-      </p>
+      </AuthLink>
     </>
   );
 }
