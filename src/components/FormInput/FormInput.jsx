fc918f732a171b783af28cf6dfc044eb10d83188
Areshka <oleg1990moskalenko@gmail.com>
2020-09-22 01:41:41 +0300

    added forwardRef

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -1,38 +1,21 @@
 import React from 'react';
 
-import './style.scss';
+import { Input } from './styled'
 
-const FormInput = ({
-  label,
-  className,
-  type,
-  name,
-  placeholder,
-  value,
-  onChange,
-  error,
-  reference
-}) => {
+const FormInput = React.forwardRef(
+  ({
+    error,
+    onChange,
+    ...props
+  }, ref) => {
 
-  const inputErrorStyle = error ? { borderColor: "rgb(243, 69, 69)" } : null;
-
-  return (
-    <div className='field-wrap'>
-      {label && <label htmlFor={name}>{label} </label>}
-      <input
-        style={inputErrorStyle}
-        id={name}
-        className={className}
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        ref={reference}
+    return (
+      <Input
+        {...props}
+        ref={ref}
+        onChange={(e) => onChange && onChange(e.target.value)}
       />
-      {error && <p className="field-wrap__error-message">{error.message || "Invalid field"}</p>}
-    </div>
-  )
-}
+    )
+  })
 
 export default FormInput;
