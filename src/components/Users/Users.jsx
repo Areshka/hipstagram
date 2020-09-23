fdf00396ffda8fe0db36c20eb60fc914ebcd7ae9
Oleg M <oleg.moskalenko@gmail.com>
2020-09-23 13:02:37 +0300

    deleted Header

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import User from './User';
-import Header from '../Header';
 import { WrapperContent } from '../Wrapper/Wrapper'
 import { ReactComponent as UsersNotFoundImg } from '../../images/icon_not_user.svg'
 
@@ -15,23 +14,20 @@ const Users = () => {
   ]
 
   return (
-    <>
-      <Header />
-      <WrapperContent>
-        {users.length ?
-          <div className='users'>
-            <User />
-            <User />
-            <User />
-          </div>
-          :
-          <NoUsersBlock>
-            <UsersNotFoundImg />
-            <p>Users not found</p>
-          </NoUsersBlock>
-        }
-      </WrapperContent>
-    </>
+    <WrapperContent>
+      {users.length ?
+        <div className='users'>
+          <User />
+          <User />
+          <User />
+        </div>
+        :
+        <NoUsersBlock>
+          <UsersNotFoundImg />
+          <p>Users not found</p>
+        </NoUsersBlock>
+      }
+    </WrapperContent>
   );
 }
 
