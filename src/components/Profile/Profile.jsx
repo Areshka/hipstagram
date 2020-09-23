5fdf30f9243c47e833a4a8c0acc371a45164d281
Oleg M <oleg.moskalenko@gmail.com>
2020-09-23 16:13:50 +0300

    added logic open Modal

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import {
   ProfileAccount,
@@ -9,6 +10,7 @@ import {
 } from './styled'
 import { FollowButton } from '../Button/Button';
 import { WrapperContent } from '../Wrapper/Wrapper';
+import { showModal } from '../../store/modal/actions';
 
 import ProfileAvatarImg from '../../images/profile-avatar.png';
 import ProfileImg1 from '../../images/profile-image-1.jpg';
@@ -19,6 +21,12 @@ import ProfileImg5 from '../../images/profile-image-5.jpg';
 import ProfileImg6 from '../../images/profile-image-6.jpg';
 
 const Profile = () => {
+  const dispatch = useDispatch()
+
+  const openModal = () => {
+    dispatch(showModal())
+  }
+
   return (
     <WrapperContent>
       <ProfileAccount>
@@ -34,11 +42,11 @@ const Profile = () => {
           <FollowButton profile='true' type="button" title="Follow" />
           <ProfileText>
             Not 23, not designer, not from California
-            </ProfileText>
+          </ProfileText>
         </ProfileInfo>
       </ProfileAccount>
       <div>
-        <img src={ProfileImg1} alt="" />
+        <img src={ProfileImg1} alt="" onClick={openModal} />
         <img src={ProfileImg2} alt="" />
         <img src={ProfileImg3} alt="" />
         <img src={ProfileImg4} alt="" />
