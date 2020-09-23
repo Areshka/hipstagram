d343de09261ffb41895f06f7c8aa50c95c278484
Areshka <oleg1990moskalenko@gmail.com>
2020-09-23 23:56:26 +0300

    delete Modal

--- src/containers/NotAuth/NotAuth.jsx
+++ src/containers/NotAuth/NotAuth.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
-import Modal from '../../components/Modal';
 import Header from '../../components/Header';
 import NotAuthRouter from './NotAuthRouter';
 
-import { isOpenModalStateSelector } from '../../store/modal/selectors'
-
 const NotAuth = () => {
-  const isOpen = useSelector(isOpenModalStateSelector);
 
   return (
     <>
       <Header />
-      <NotAuthRouter />
-      {isOpen && <Modal message="ASdfghjj" />}
+      <NotAuthRouter />     
     </>
   );
 }
