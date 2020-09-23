b066091b369cac62b039a050a6a2c64e501a090b
Oleg M <oleg.moskalenko@gmail.com>
2020-09-23 16:13:02 +0300

    added logic open Modal

--- src/containers/Application/Application.jsx
+++ src/containers/Application/Application.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
+import Modal from '../../components/Modal';
 import Header from '../../components/Header';
 import ApplicationRouter from './ApplicationRouter';
 
+import { isOpenModalStateSelector } from '../../store/modal/selectors'
+
 const Application = () => {
+  const isOpen = useSelector(isOpenModalStateSelector);
+
   return (
     <>
       <Header />
       <ApplicationRouter />
+      {isOpen && <Modal message="ASdfghjj" />}
     </>
   );
 }
