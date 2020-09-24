f9af4e5bfc98b7180558431ed8d6406181ac4f0a
Areshka <oleg1990moskalenko@gmail.com>
2020-09-25 00:15:57 +0300

    added registration request

--- src/api/auth.service.js
+++ src/api/auth.service.js
@@ -4,3 +4,8 @@ export const loginFetch = async userData => {
   const {data} = await axios.post('/auth/login', userData);
   return data;
 }
+
+export const registrationFetch = async userData => {
+  const {data} = await axios.post('/auth/reg', userData);
+  return data;
+}
