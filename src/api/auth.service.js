489a1461b604e2cf01fa7f014cf00464ea08a337
Areshka <oleg1990moskalenko@gmail.com>
2020-09-27 23:21:54 +0300

    added interceptors

--- src/api/auth.service.js
+++ src/api/auth.service.js
@@ -1,11 +1,40 @@
 import axios from './axios.instence';
+import { toast } from "react-toastify";
+
+import store from '../store/store';
+import { logoutThunk } from '../store/auth/thunks';
+
+axios.interceptors.request.use(
+  function (config) {
+    config.headers = {
+      ...config.headers,
+      Authorization: localStorage.getItem('access_token'),
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  function (response) {    
+    return response;
+  },
+  function (error) {
+    toast.error(error.response.data);
+    store.dispatch(logoutThunk());
+    return Promise.reject(error);
+  }
+);
 
 export const loginFetch = async userData => {
-  const {data} = await axios.post('/auth/login', userData);
+  const { data } = await axios.post('/auth/login', userData);
   return data;
 }
 
 export const registrationFetch = async userData => {
-  const {data} = await axios.post('/auth/registration', userData);
+  const { data } = await axios.post('/auth/registration', userData);
   return data;
 }
