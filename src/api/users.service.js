b81b4b6f1a12f6f08c0f712ea004e27f251067e0
Areshka <oleg1990moskalenko@gmail.com>
2020-09-28 22:16:59 +0300

    added a request to change the password

--- src/api/users.service.js
+++ src/api/users.service.js
@@ -4,3 +4,8 @@ export const currentUserFetch = async () => {
   const { data } = await axios.get('/users/current');
   return data;
 }
+
+export const updatePasswordFetch = async passwords => {
+  const { data } = await axios.post('/auth/updatePassword', passwords);
+  return data;
+}
