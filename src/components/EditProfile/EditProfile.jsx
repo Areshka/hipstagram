cb78bbb97021d4446f49178ff61bb082a4108f6d
Areshka <oleg1990moskalenko@gmail.com>
2020-09-29 00:50:19 +0300

    added EditProfile component

---
+++ src/components/EditProfile/EditProfile.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { ToastContainer } from "react-toastify";
+import { useDispatch, useSelector } from 'react-redux';
+
+import FormInput from '../FormInput';
+import { Button } from '../Button/Button';
+import FieldWrapper from '../FieldWrapper';
+
+import { nameValidation, emailValidationData } from '../../constants/formPatterns';
+import { updateCurrentUserThunk } from '../../store/users/thunks';
+import { getCurrentUserSelector } from '../../store/users/selectors';
+
+import "react-toastify/dist/ReactToastify.css";
+
+const EditProfile = () => {
+  const dispatch = useDispatch()
+  const currentUser = useSelector(getCurrentUserSelector);
+  const { register, errors, handleSubmit, setValue } = useForm();
+
+  useEffect(() => {
+    setValue("firstName", currentUser.firstName)
+    setValue("lastName", currentUser.lastName)
+    setValue("email", currentUser.email)
+  }, [currentUser])
+
+  const onSubmit = (data) => {
+    dispatch(updateCurrentUserThunk(data))
+  }
+
+  return (
+    <>
+      <ToastContainer />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FieldWrapper label="First name" error={errors.firstName || ''}>
+          <FormInput
+            primary
+            type="text"
+            name="firstName"
+            placeholder="Enter first name"
+            invalid={'firstName' in errors}
+            ref={register(nameValidation)}
+          />
+        </FieldWrapper>
+
+        <FieldWrapper label="Last name" error={errors.lastName || ''}>
+          <FormInput
+            primary
+            type="text"
+            name="lastName"
+            placeholder="Enter last name"
+            invalid={'lastName' in errors}
+            ref={register(nameValidation)}
+          />
+        </FieldWrapper>
+
+        <FieldWrapper label="Email" error={errors.email || ''}>
+          <FormInput
+            primary
+            type="email"
+            name="email"
+            placeholder="Enter your Email"
+            invalid={'email' in errors}
+            ref={register(emailValidationData)}
+          />
+        </FieldWrapper>
+
+        <Button className="btn" type="submit" title="Update" />
+      </form>
+    </>
+  );
+}
+
+export default EditProfile;
