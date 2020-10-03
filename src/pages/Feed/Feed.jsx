3ca0ddf79e72b07b1ab603f5554ffc7632902d63
Areshka <oleg1990moskalenko@gmail.com>
2020-10-03 16:15:26 +0300

    added components Feed

---
+++ src/pages/Feed/Feed.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header';
+import { WrapperContent } from '../../components/Wrapper/Wrapper';
+
+const Feed = () => {
+  return (
+    <>
+      <Header />
+      <WrapperContent>
+        <h1>Feed</h1>
+      </WrapperContent>
+    </>
+  );
+}
+
+export default Feed;
