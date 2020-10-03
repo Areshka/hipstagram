1df05ae7b4819e6d778c8ca9145092fbc67087b8
Areshka <oleg1990moskalenko@gmail.com>
2020-10-03 22:40:00 +0300

    created function redirectToProfile

--- src/modules/NewPost/NewPost.jsx
+++ src/modules/NewPost/NewPost.jsx
@@ -10,22 +10,31 @@ import { DefaultButton } from '../../components/Button/Button';
 import { WrapperContent } from '../../components/Wrapper/Wrapper';
 
 import { createPostThunk } from '../../store/users/thunks';
+import { fileValidation } from '../../constants/formPatterns';
 
 const NewPost = () => {
   const dispatch = useDispatch();
-  const history = useHistory();
-  const { register, handleSubmit } = useForm();
-
-  const redirectToProfile = () => {
-    history.push('/profile');
-  }
+  let history = useHistory();
+  const { register, handleSubmit, errors, setError } = useForm();
+  console.log(history)
+  const redirectToProfile = () => history.push('/profile');
 
   const onSubmit = ({ file, title }) => {
     const image = file[0];
     const formData = new FormData();
     formData.append('image', image);
     formData.append('title', title);
-    dispatch(createPostThunk(formData, redirectToProfile))
+
+    if (image.size > 2e6) {
+      setError("file", {
+        type: "filesize",
+        message: "File size should be no more than 2 Mb"
+      });
+      return;
+    }
+
+    dispatch(createPostThunk({ formData, redirectToProfile }))
+    // history.push('/profile')
   }
 
   return (
@@ -33,20 +42,22 @@ const NewPost = () => {
       <Header />
       <WrapperContent>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <FieldWrapper>
+          <FieldWrapper error={errors.file || ''}>
             <FormInput
               type="file"
               name="file"
-              ref={register()}
+              invalid={'file' in errors}
+              ref={register(fileValidation)}
             />
           </FieldWrapper>
 
-          <FieldWrapper>
+          <FieldWrapper error={errors.title || ''}>
             <FormInput
               type="text"
               name="title"
               placeholder="Enter title"
-              ref={register()}
+              invalid={'title' in errors}
+              ref={register({ required: true })}
             />
           </FieldWrapper>
 
