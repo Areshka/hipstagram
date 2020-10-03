49baad9767a86a22ebb398a85abb8d4c716527ca
Areshka <oleg1990moskalenko@gmail.com>
2020-10-03 16:15:55 +0300

    added route to Feed

--- src/containers/NotAuth/NotAuthRouter.jsx
+++ src/containers/NotAuth/NotAuthRouter.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect, Switch } from "react-router-dom";
 
+import Feed from '../../pages/Feed';
 import Users from '../../modules/Users';
 import Profile from '../../pages/Profile';
 import NewPost from '../../modules/NewPost';
@@ -9,12 +10,13 @@ import Settings from '../../pages/Settings';
 const NotAuthRouter = () => {
   return (
     <Switch>
+      <Route exact path='/' component={Feed} />
       <Route path='/users' component={Users} />
       <Route path='/profile' component={Profile} />
       <Route path="/profile/:id" component={Profile} />
       <Route path="/settings" component={Settings} />
       <Route path='/new_post' component={NewPost} />
-      <Redirect to='/profile' />
+      <Redirect to='/' />
     </Switch>
   );
 }
