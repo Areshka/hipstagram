6e1490d5b6d90668e9c9e03029224db398fa95bb
Areshka <oleg1990moskalenko@gmail.com>
2020-11-19 00:33:44 +0200

    HashRouter

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from "react-router-dom";
-import store from '../../store/store';
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, HashRouter } from "react-router-dom";
+import store from "../../store/store";
 
 const Layout = ({ children }) => {
   return (
     <Provider store={store}>
-      <Router>
-        {children}
-      </Router>
+      <HashRouter basename="/">{children}</HashRouter>
     </Provider>
   );
-}
+};
 
 export default Layout;
