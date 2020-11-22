570a0ca40b838ee1ccee1e8933b9b33642106a2b
Areshka <oleg1990moskalenko@gmail.com>
2020-11-23 00:20:22 +0200

    Changed Preloader

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { StyledPreloader } from './styled';
+import React from "react";
+import { StyledPreloader } from "./styled";
 
 const Preloader = () => {
   return (
     <StyledPreloader>
-      <img src="https://i.gifer.com/origin/ec/ecf46fc2a40f43ad0ef438b04b0d2e8e_w200.gif" alt="preloader" />
+      {/* <img src="https://i.gifer.com/origin/ec/ecf46fc2a40f43ad0ef438b04b0d2e8e_w200.gif" alt="preloader" /> */}
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </StyledPreloader>
-  )
-}
+  );
+};
 
 export default Preloader;
